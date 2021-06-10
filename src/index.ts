7d2528b02d57cf02017e05b0d60df874105d986b
Walter Corrales <corraleswalter@live.com>
2021-06-09 23:33:41 -0600

    [🔨] code improvements

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { flow } from "./flow";
+import { flow } from "./utils/flow";
 import { ConnectBase } from "./connect";
 import { EmployeeService } from "./services/employee-service";
 import { PayrollService } from "./services/payroll-service";
@@ -10,9 +10,5 @@ export { EmployeeLevel } from "./types/EmployeeLevel";
 export { EmployeePaygroup } from "./types/EmployeePaygroup";
 export { EmployeeSchedule } from "./types/EmployeeSchedule";
 
-export class AsureForceConnect extends EmployeeService(
-    TimecardService(PayrollService(ConnectBase))
-) {}
-
 const mixer = flow(EmployeeService, TimecardService, PayrollService);
-export class AsureForceConnect2 extends mixer(ConnectBase) {}
+export class AsureForceConnect extends mixer(ConnectBase) { }
